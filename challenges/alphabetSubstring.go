816e768d560be9ad4c8d593c1e6820cdaedaee2c
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2019-06-29 00:26:11 -0700

    modify solution for alphabetSubstring challenge

--- challenges/alphabetSubstring.go
+++ challenges/alphabetSubstring.go
@@ -1,8 +1,5 @@
+import "strings"
+
 func alphabetSubstring(s string) bool {
-	for i := 1; i < len(s); i++ {
-		if int(s[i])-int(s[i-1]) != 1 {
-			return false
-		}
-	}
-	return true
+	return strings.Contains("abcdefghijklmnopqrstuvwxyz", s)
 }
